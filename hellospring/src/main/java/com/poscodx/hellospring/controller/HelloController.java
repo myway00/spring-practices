69c4479802ce6b46a9eec2c50372fc12e27c23a8
myway00 <junior0614@naver.com>
2023-09-14 17:25:14 +0900

    init

--- hellospring/src/main/java/com/poscodx/hellospring/controller/HelloController.java
+++ hellospring/src/main/java/com/poscodx/hellospring/controller/HelloController.java
@@ -5,8 +5,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HelloController {
-	@RequestMapping("/hello")
-	public String hello() {
-		return "/WEB-INF/views/hello.jsp";
-	}
+    @RequestMapping("/hello")
+    public String hello() {
+        return "/WEB-INF/views/hello.jsp";
+    }
 }
